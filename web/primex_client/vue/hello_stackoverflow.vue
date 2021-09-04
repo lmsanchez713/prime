d0ff0cdf808916c7391b6c580a7487c77374a7a1
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-09-04 13:41:24 -0300

    ~

---
+++ web/primex_client/vue/hello_stackoverflow.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Hello</p>
+
+  <p>{{ message }}</p>
+</template>
+
+<script>
+module.exports = {
+  data: {
+    message: "hello",
+  },
+};
+</script>
+
+<style>
+p {
+  font-size: 14px;
+}
+</style>
