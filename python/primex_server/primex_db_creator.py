3c659433b3793501eac4c26dfe71b785f54f7f5b
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-09-22 18:34:02 -0300

    ~

--- python/primex_server/primex_db_creator.py
+++ python/primex_server/primex_db_creator.py
@@ -7,7 +7,7 @@ with open('/code/mysql.json', 'r') as arquivo_json_mysql:
     credenciais_mysql = json.load(arquivo_json_mysql)
     mysql_db = mysql.connector.connect(host="localhost", user=credenciais_mysql["usuario"], password=credenciais_mysql["senha"])
     mysql_cursor = mysql_db.cursor()
-    mysql_cursor.execute("DROP DATABASE primex;")
+    mysql_cursor.execute("DROP DATABASE IF EXISTS primex;")
     mysql_cursor.execute("CREATE DATABASE primex;")
     mysql_cursor.execute("USE primex;")
     mysql_cursor.execute("CREATE TABLE usuarios(id_usuario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT);")
