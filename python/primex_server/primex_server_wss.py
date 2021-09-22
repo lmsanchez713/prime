2d49a6910b2d67cca53436915b400a8a26e36230
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-09-22 00:19:32 -0300

    ~

--- python/primex_server/primex_server_wss.py
+++ python/primex_server/primex_server_wss.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-# WSS (WS over TLS) server example, with a self-signed certificate
-
-# start_server = websockets.serve(primex_main, "140.82.31.140", 9713, ssl=ssl_context)
-
 import asyncio
 import websockets
 import sys
-# import pathlib
 import ssl
 import hashlib
 import json
@@ -26,22 +21,9 @@ async def primex_main(websocket, path):
 
         requisicao = json.loads(mensagem)
 
-        print(type(requisicao))
-        # print("PRINT DIR")
-        # print(dir(requisicao))
-        # print("PRINT VARS")
-        # print(vars(requisicao))
-        # print("PRINT DICT")
-        # print(dict(requisicao))
-        # print("PPRINT DIR")
-        # pprint(dir(requisicao))
-        # print("PPRINT VARS")
-        # pprint(vars(requisicao))
-        # print("PPRINT DICT")
-        # pprint(dict(requisicao))
-
         req_id = requisicao["req_id"]
         resposta = f'{{"req_id":{req_id},"status":"erro","mensagem":"Servidor em manutenção"}}'
+
         await websocket.send(resposta)
         print(f"requisicao respondida: {resposta}")
 
@@ -52,11 +34,8 @@ async def server_proc(parada, ssl_context):
 async def main(parada, server_ws):
     saida_solicitada = False
     while saida_solicitada == False:
-        # entrada = input("> ")
         entrada = await ainput("")
         entrada = entrada[:-1]
-        # print(entrada)
-        # print(len(entrada))
         if entrada == "exit":
             print("Saída solicitada")
             saida_solicitada = True
@@ -70,21 +49,14 @@ print("Digite seu comando ou 'exit' para sair")
 loop = asyncio.get_event_loop()
 # loop.set_debug(True)
 
-# start_server = websockets.serve(primex_main, "127.0.0.1", 9713)
-
 parada = loop.create_future()
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
-# certfile = pathlib.Path(__file__).with_name("/var/www/certs/formatafacil.com.br/cert-chain.crt")
-# keyfile = pathlib.Path(__file__).with_name("/var/www/certs/formatafacil.com.br/cert.key")
-# ssl_context.load_cert_chain(certfile, keyfile)
 # ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
 ssl_context.minimum_version = ssl.TLSVersion.MAXIMUM_SUPPORTED
 ssl_context.load_cert_chain("/var/www/certs/formatafacil.com.br/cert-chain.crt", "/var/www/certs/formatafacil.com.br/cert.key")
 
 server_ws = loop.create_task(server_proc(parada, ssl_context))
 loop.run_until_complete(main(parada, server_ws))
-# asyncio.get_event_loop().run_forever()
 
 print("Saindo do servidor... Obrigado!")
