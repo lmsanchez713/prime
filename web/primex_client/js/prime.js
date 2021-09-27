35abb3ceedc1826e020f1127ff88fe313602368b
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2021-09-27 02:06:32 -0300

    ~

--- web/primex_client/js/prime.js
+++ web/primex_client/js/prime.js
@@ -249,7 +249,7 @@ function inicializar() {
 
 }
 
-$("#modal-login").on('hidden.bs.modal', function () {
+$("#modal-login").on('hide.bs.modal', function () {
     remover_status_login_invalido();
     remover_status_senha_invalida();
     alert("HEY");
